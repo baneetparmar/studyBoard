54c2f8edbeef6a4569d57f0bd40ae9383431cae2
Baneet Parmar <baneetparmar7@gmail.com>
2024-08-09 09:37:34 +0530

    :sparkles: Add access restrictions

--- studyBoard/base/views.py
+++ studyBoard/base/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") is not None else ""
@@ -24,7 +25,7 @@ def room(request, pk):
 
     return render(request, "base/room.html", context)
 
-
+@login_required(login_url='/login')
 def createRoom(request):
     form = RoomForm()
     if request.method == "POST":
@@ -36,10 +37,14 @@ def createRoom(request):
     context = {"form": form}
     return render(request, "base/room_form.html", context)
 
-
+@login_required(login_url='/login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.user:
+        return HttpResponse("You are not allowed to")
+
     if request.method == "POST":
         form = RoomForm(request.POST, instance=room)
         if form.is_valid():
@@ -59,6 +64,9 @@ def deleteRoom(request, pk):
 
 
 def loginPage(request):
+    if request.user.is_authenticated():
+        return redirect('home')
+
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
