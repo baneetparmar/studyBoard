77f7936063374cc13c7182849d2377ab011cd12a
Baneet Parmar <baneetparmar7@gmail.com>
2024-08-11 15:00:45 +0530

    :sparkles: Add CRUD operations for chat room messages

--- studyBoard/base/views.py
+++ studyBoard/base/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+
 
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") is not None else ""
@@ -15,17 +17,32 @@ def home(request):
     )
     topics = Topic.objects.all()
     room_count = rooms.count()
+
     context = {"rooms": rooms, "topics": topics, "room_count": room_count}
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {"room": room}
+    room_messages = room.message_set.all().order_by("-created")
+    participants = room.participants.all()
+    if request.method == "POST":
+        Message.objects.create(
+            user=request.user, room=room, body=request.POST.get("body")
+        )
+        room.participants.add(request.user)
+        return redirect("room", pk=room.id)
+
+    context = {
+        "room": room,
+        "room_messages": room_messages,
+        "participants": participants,
+    }
 
     return render(request, "base/room.html", context)
 
-@login_required(login_url='/login')
+
+@login_required(login_url="/login")
 def createRoom(request):
     form = RoomForm()
     if request.method == "POST":
@@ -37,7 +54,8 @@ def createRoom(request):
     context = {"form": form}
     return render(request, "base/room_form.html", context)
 
-@login_required(login_url='/login')
+
+@login_required(login_url="/login")
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
@@ -63,9 +81,10 @@ def deleteRoom(request, pk):
     return render(request, "base/delete.html", {"obj": room})
 
 
-def loginPage(request):
-    if request.user.is_authenticated():
-        return redirect('home')
+def loginUser(request):
+    page = "login"
+    if request.user.is_authenticated:
+        return redirect("home")
 
     if request.method == "POST":
         username = request.POST.get("username")
@@ -76,7 +95,7 @@ def loginPage(request):
         except Exception as err:
             print(err)
             messages.error(request, "user does not exist")
-            return redirect('login')  # TODO
+            return redirect("login")  # TODO
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
@@ -86,10 +105,43 @@ def loginPage(request):
                 request, "username or password is incorrect or user does not exist"
             )
 
-    context = {}
+    context = {"page": page}
+    return render(request, "base/login_register.html", context)
+
+
+def registerUser(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        messages.error(
+            request, "An error occurred during registeration. Please try again."
+        )
+
+    context = {"form": form}
     return render(request, "base/login_register.html", context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect("home")
+
+
+@login_required(login_url="login")
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse("Access Denied!")
+    if request.method == "POST":
+        message.delete()
+        return redirect("home")
+    context = {"obj": message}
+    return render(request, "base/delete.html", context)
--- treefmt.nix
+++ treefmt.nix
@@ -1,7 +1,14 @@
 { pkgs, ... }:
 {
   projectRootFile = ".git/config";
+
   programs.ruff.enable = true;
+  settings.formatter.ruff = {
+    command = "${pkgs.ruff}/bin/ruff";
+    includes = [ "*.py" ];
+    options = [ "format" ];
+  };
+
   programs.nixfmt.enable = true;
   programs.prettier.enable = true;
 }
