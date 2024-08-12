4329b165caff98b576e8dab8feb3522fe2b3fa15
Baneet Parmar <baneetparmar7@gmail.com>
2024-08-12 18:24:26 +0530

    :art: Refactor activity, feed and topics into seperate components

--- studyBoard/base/views.py
+++ studyBoard/base/views.py
@@ -17,14 +17,20 @@ def home(request):
     )
     topics = Topic.objects.all()
     room_count = rooms.count()
+    activity_feed = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
+    context = {
+        "rooms": rooms,
+        "topics": topics,
+        "room_count": room_count,
+        "activity_feed": activity_feed,
+    }
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by("-created")
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == "POST":
         Message.objects.create(
